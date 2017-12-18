f62e2a663944b305e5b32f3c6317c4a2dcc489b3
Andreas Sjoberg <ansj@bluegarden.com>
2017-12-18 10:26:54 +0100

    Over advanced solution simplified

--- day16.py
+++ day16.py
@@ -9,11 +9,8 @@ def swap(array, index_a, index_b):
     array[index_b] = tmp
 
 
-def part_one(data):
-    """Part one"""
-    array = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
-             'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-    commands = data.split(',')
+def dance(array, commands):
+    """Do the dance"""
     for command in commands:
         if command[0] == 's':
             spin_size = int(command[1:])
@@ -27,6 +24,14 @@ def part_one(data):
             swap_a, swap_b = command[1:].split('/')
             index_a, index_b = array.index(swap_a), array.index(swap_b)
             swap(array, index_a, index_b)
+
+
+def part_one(data):
+    """Part one"""
+    array = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
+             'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+    commands = data.split(',')
+    dance(array, commands)
     return ''.join(map(str, array))
 
 
@@ -34,26 +39,9 @@ def part_two(data):
     """Part two"""
     array = ['a', 'b', 'c', 'd', 'e', 'f', 'g',
              'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
-    reverse_dict = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6,
-                    'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15}
     commands = data.split(',')
     for _ in range(1000000000 % 30):
-        for command in commands:
-            if command[0] == 's':
-                spin_size = int(command[1:])
-                for key, value in reverse_dict.iteritems():
-                    reverse_dict[key] = (value + spin_size) % 16
-                array = sorted(reverse_dict, key=reverse_dict.get)
-            elif command[0] == 'x':
-                index_a, index_b = command[1:].split('/')
-                char_a, char_b = array[int(index_a)], array[int(index_b)]
-                swap(array, int(index_a), int(index_b))
-                swap(reverse_dict, char_a, char_b)
-            elif command[0] == 'p':
-                swap_a, swap_b = command[1:].split('/')
-                index_a, index_b = reverse_dict[swap_a], reverse_dict[swap_b]
-                swap(array, index_a, index_b)
-                swap(reverse_dict, swap_a, swap_b)
+        dance(array, commands)
     return ''.join(map(str, array))
 
 
